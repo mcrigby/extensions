94bf0701ab8bbd90e6905af9f14f17fb63538078
Martin Cutillo-Rigby <70958005+mcrigby@users.noreply.github.com>
2020-05-17 22:20:05 +0100

    removed redundant extension. added some others.

--- CommonExtensions/CommonExtensions.cs
+++ CommonExtensions/CommonExtensions.cs
@@ -1,13 +1,32 @@
-﻿namespace System
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace System
 {
     public static class CommonExtensions
     {
-        public static TSource ThrowIfNull<TSource>(this TSource source, string sourceName)
+        public static string ToTitleCase(this string source)
         {
-            if ( source == null)
-                throw new ArgumentNullException(sourceName);
+            var parts = source.Split(' ')
+                .Select(part =>
+                {
+                    if (string.IsNullOrWhiteSpace(part))
+                        return part;
+                    if (part.Length == 1)
+                        return part.ToUpper();
 
-            return source;
+                    return part.Substring(0, 1).ToUpper() + part.Substring(1).ToLower();
+                });
+
+            return string.Join(" ", parts);
+        }
+
+        public static string ToSnakeCase(this string input)
+        {
+            if (string.IsNullOrEmpty(input)) { return input; }
+
+            var startUnderscores = Regex.Match(input, @"^_+");
+            return startUnderscores + Regex.Replace(input, @"([a-z0-9])([A-Z])", "$1_$2").ToLower();
         }
     }
 }
